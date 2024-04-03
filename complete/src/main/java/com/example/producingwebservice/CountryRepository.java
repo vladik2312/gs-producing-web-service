133719ac66dd97930d10dfc34887b4d587feca99
Voland <Tereshenkovladik@gmail.com>
2024-04-04 02:13:11 +0300

    Changes by user

--- complete/src/main/java/com/example/producingwebservice/CountryRepository.java
+++ complete/src/main/java/com/example/producingwebservice/CountryRepository.java
@@ -1,47 +1,10 @@
 package com.example.producingwebservice;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.JpaRepository;
 import io.spring.guides.gs_producing_web_service.Country;
-import io.spring.guides.gs_producing_web_service.Currency;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Component;
-import org.springframework.util.Assert;
 
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
-import java.util.List;
+public interface CountryRepository extends JpaRepository<Country, Integer> {
 
-@Component
-public class CountryRepository {
-	private final JdbcTemplate jdbcTemplate;
-
-	@Autowired
-	public CountryRepository(DataSource dataSource) {
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
-	}
-
-	@PostConstruct
-	public void initData() {
-		// Initialization not needed since data is retrieved from the database
-	}
-
-	public Country findCountry(String name) {
-		Assert.notNull(name, "The country's name must not be null");
-
-		String sql = "SELECT * FROM Country WHERE name = ?";
-		List<Country> countries = jdbcTemplate.query(sql, new Object[]{name}, (rs, rowNum) -> {
-			Country country = new Country();
-			country.setName(rs.getString("name"));
-			country.setCapital(rs.getString("capital"));
-			country.setCurrency(Currency.valueOf(rs.getString("currency")));
-			country.setPopulation(rs.getInt("population"));
-			return country;
-		});
-
-		if (countries.isEmpty()) {
-			return null;
-		}
-
-		return countries.get(0);
-	}
+    Country findCountriesByName(String name);
 }
